055c6fac996022a6565607c55bd25a4e48fdf71b
Carlos Garcia <decca2122@gmail.com>
2019-05-30 15:28:37 +0000

    plz fix

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ var apiKey = "&api_key=7424bc8327fbe32ea30c48abefd6ebff&format=json";
     
     
     // var apiRequestURL = baseURL + songApiMethod + searchTerm + apiKey;
-    var apiRequestURL = adamApi + searchTerm
+    var apiRequestURL = adamApi +  searchTerm;
     
     // console.log(apiRequestURL);
     
@@ -36,15 +36,44 @@ var apiKey = "&api_key=7424bc8327fbe32ea30c48abefd6ebff&format=json";
 });
 
 
+//test
+
+$('#artist-button').click(function() {
+ var artistButton = $("#artist").val();
+    
+    
+    
+ // var apiRequestURL = baseURL + songApiMethod + searchTerm + apiKey;
+ var apiRequestURL = adamApi +  artistButton
+    
+ // console.log(apiRequestURL);
+    
+    
+ $.ajax({
+   url: apiRequestURL,
+   method: "GET",
+   success: function(response) {
+     console.log(response[0].embed_url);
+     // var music = response.results.trackmatches.track[0].url
+      $("#some-text").html('<iframe id="player" type="text/html" width="640" height="390" src=' + response[0].embed_url + 'frameborder="0"></iframe>');
+          
+     // console.log(music);
+     // var artist= response.results.trackmatches.track[]
+   },
+ }); 
+
+
+//test
+
 
 $("#artist").keyup(function(event) {
     if (event.keyCode === 13) {
-        $("#search-button").click();
+        $("#artist-button").click();
     }
 });
 
 $("#search-term").keyup(function(event) {
     if (event.keyCode === 13) {
-        $("#searche-button").click();
+        $("#search-button").click();
     }
 });
